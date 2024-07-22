7a794674f7f328204a8229830e7f3e69445af819
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-07-22 11:09:39 -0400

    Create oldest company method

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,12 @@ class Company < ActiveRecord::Base
   has_many :freebies
   has_many :devs, through: :freebies
 
+  def give_freebie(dev, item_name, value)
+
+  end
+
+  def self.oldest_company
+    order(:founding_year).first
+  end
+
 end
